4dd9dac44c52ec1f2f575d357aa55a538af52137
Leonardo S Coelho <leonardo_soares29@hotmail.com>
2022-06-13 16:39:29 +0200

    Better class workflow config

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import { Invoice } from "./classes/Invoice.js";
+
+const invOne = new Invoice("mario", "work on mario website", 250);
+const invTwo = new Invoice("luigi", "work on luigi website", 300);
+
+let invoices: Invoice[] = [];
+
+invoices.push(invOne);
+invoices.push(invTwo);
+
+console.log(invoices);
